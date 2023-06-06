b6548e3c55dea38ac6da0d7806ca3df18db0a6fb
md-asif-iqbal <miqbal191034@bscse.uiu.ac.bd>
2023-06-06 21:02:11 +0600

    check error

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv'
-import path from 'path'
-dotenv.config({ path: path.join(process.cwd(), '.env') })
+// import dotenv from 'dotenv'
+// import path from 'path'
+// dotenv.config({ path: path.join(process.cwd(), '.env') })
 
-export default {
-  port: process.env.PORT,
-  database_urls: process.env.DATABASE_URL,
-  default_user_pass: process.env.DEFAULT_USER_PASS,
-}
+// export default {
+//   port: process.env.PORT,
+//   database_urls: process.env.DATABASE_URL,
+//   default_user_pass: process.env.DEFAULT_USER_PASS,
+// }
 
-// djfbjdef
+// djfbjdefgg
